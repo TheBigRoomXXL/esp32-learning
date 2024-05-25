4cbaa75b86bf5e2424933ccc12c61d29216f727c
TheBigRoomXXL <seb.lovergne@gmail.com>
2024-05-25 09:05:30 +0200

    wip

--- tintinker/main/tintinker.c
+++ tintinker/main/tintinker.c
@@ -16,11 +16,19 @@ void app_main(void) {
   ESP_LOGI(ourTaskName, "hello, starting up");
 
   gpio_reset_pin(BLINK_LED);
-  gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
+  int test = gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
+
+  ESP_LOGI(ourTaskName, "gpio_set_direction result : %d", test);
+  gpio_dump_io_configuration(stdout, BLINK_LED);
+
   while (1) {
     gpio_set_level(BLINK_LED, 1);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
+
     gpio_set_level(BLINK_LED, 0);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
+
+// I (293) gpio: GPIO[8]| InputEn: 0| OutputEn: 1| OpenDrain: 0| Pullup: 1| Pulldown: 0| Intr:0 
+// I (293) gpio: GPIO[8]| InputEn: 0| OutputEn: 0| OpenDrain: 0| Pullup: 1| Pulldown: 0| Intr:0
